3e8e499276bbc19ae19a7b949ab62ef2f5e64a8b
alwayshannonperry-cpu <alwayshannonperry@gmail.com>
2026-03-08 16:19:44 -0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -6,8 +6,8 @@ method: "POST",
 
 headers: {
 "Content-Type": "application/json",
-"Authorization": "Bearer YOUR_OPENAI_KEY"
-},
+"Authorization": "bearer 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
+},"
 
 body: JSON.stringify({
 model: "gpt-4o-mini",
