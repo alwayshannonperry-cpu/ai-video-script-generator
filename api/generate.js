77523f3f0f5e1cb9763a14ddc093580ea9d61da0
alwayshannonperry-cpu <alwayshannonperry@gmail.com>
2026-03-10 06:18:24 -0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,10 +1,6 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
 
-const { topic } = req.body || {}
-
-if (!topic) {
-return res.status(400).json({ text: "Enter a topic first" })
-}
+const topic = req.body?.topic || "something interesting"
 
 const script = `
 HOOK:
@@ -12,15 +8,15 @@ Stop scrolling if you want to learn about ${topic}.
 
 SCRIPT:
 Here are three quick tips about ${topic}.
-Tip one: start simple.
-Tip two: stay consistent.
-Tip three: keep improving every day.
+1. Start simple
+2. Be consistent
+3. Improve every day
 
 CAPTION:
 Quick tips about ${topic} you should try today.
 
 HASHTAGS:
-#tips #learn #${topic.replace(/\s/g,"")}
+#viral #tips #${topic.replace(/\s/g,"")}
 `
 
 res.status(200).json({ text: script })
