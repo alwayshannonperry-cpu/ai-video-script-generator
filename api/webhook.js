41de3665005034d80a80789d9676a416f45f6920
alwayshannonperry-cpu <alwayshannonperry@gmail.com>
2026-04-05 03:47:25 -0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,18 +1,29 @@
 import Stripe from "stripe";
+import { addUser } from "./users";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-let proUsers = [];
+export const config = {
+api: {
+bodyParser: false,
+},
+};
 
-export default async function handler(req, res) {
+export default async function handler(req, res){
 
 const sig = req.headers["stripe-signature"];
+
+let buf = await new Promise(resolve => {
+let data = [];
+req.on("data", chunk => data.push(chunk));
+req.on("end", () => resolve(Buffer.concat(data)));
+});
 
 let event;
 
 try {
 event = stripe.webhooks.constructEvent(
-req.body,
+buf,
 sig,
 process.env.STRIPE_WEBHOOK_SECRET
 );
@@ -21,18 +32,16 @@ return res.status(400).send(`Webhook Error: ${err.message}`);
 }
 
 // ✅ WHEN PAYMENT SUCCEEDS
-if (event.type === "checkout.session.completed") {
+if(event.type === "checkout.session.completed"){
 
 const session = event.data.object;
 
-// 👇 this grabs customer email
 const email = session.customer_details.email;
 
-// add to pro users
-proUsers.push(email);
+addUser(email);
 
-console.log("NEW PRO USER:", email);
+console.log("PRO USER ADDED:", email);
 }
 
-res.status(200).json({ received: true });
+res.status(200).json({received:true});
 }
