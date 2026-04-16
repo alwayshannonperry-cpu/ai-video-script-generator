1bf6bf7557080b4faa345b239daaf67031f82a63
alwayshannonperry-cpu <alwayshannonperry@gmail.com>
2026-04-16 07:24:04 -0400

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import { paidUsers } from "@/lib/store";
+import { paidUsers } from "../../../lib/store";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
